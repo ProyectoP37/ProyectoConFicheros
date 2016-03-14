5ff24952ce2d42b00e11b51227294b31c4b1b79b
mario <mario@Lenovo-PC>
2016-03-14 16:15:28 +0100

    ficheros

---
+++ src/Ficheros/Ler.java
@@ -0,0 +1,12 @@
+
+package Ficheros;
+import com.mario.Ficheros;
+/**
+ *
+ * @author mario
+ */
+public class Ler {
+    public static void main(String args[]) {
+    Ficheros fich=new Ficheros();
+    fich.lerFich(null);
+}}
